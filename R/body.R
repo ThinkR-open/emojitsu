3eaceafc2619b3adcfdbd95ed7801500a355c737
Romain Francois <romain@thinkr.fr>
2017-08-01 18:42:28 +0200

    emojis in the body section, e.g. selfie(), flexed_biceps(), ...
    `folded_hands() %>% dark_skin_tone()` -> 🙏🏿
    `index_pointing_up() %>% light_skin_tone()` -> ☝🏻

---
+++ R/body.R
@@ -0,0 +1,165 @@
+
+#' Body emojis
+#' @export
+#' @rdname body_sequence
+selfie <- function() emoji( from_runes("U+1F933") )
+
+#' @export
+#' @rdname body_sequence
+flexed_biceps <- function() emoji( from_runes("U+1F4AA") )
+
+#' @export
+#' @rdname body_sequence
+backhand_index_pointing_left <- function() emoji( from_runes("U+1F448") )
+
+#' @export
+#' @rdname body_sequence
+backhand_index_pointing_right <- function() emoji( from_runes("U+1F449") )
+
+#' @export
+#' @rdname body_sequence
+backhand_index_pointing_down <- function() emoji( from_runes("U+1F447") )
+
+#' @export
+#' @rdname body_sequence
+backhand_index_pointing_up <- function() emoji( from_runes("U+1F446") )
+
+#' @export
+#' @rdname body_sequence
+index_pointing_up <- function() emoji( from_runes("U+261D") )
+
+#' @export
+#' @rdname body_sequence
+middle_finger <- function() emoji( from_runes("U+1F595") )
+
+#' @export
+#' @rdname body_sequence
+victory_hand <- function() emoji( from_runes("U+270C") )
+
+#' @export
+#' @rdname body_sequence
+crossed_fingers <- function() emoji( from_runes("U+1F91E") )
+
+#' @export
+#' @rdname body_sequence
+vulcan_salute <- function() emoji( from_runes("U+1F596") )
+
+#' @export
+#' @rdname body_sequence
+sign_of_the_horns <- function() emoji( from_runes("U+1F918") )
+
+#' @export
+#' @rdname body_sequence
+call_me_hand <- function() emoji( from_runes("U+1F919") )
+
+#' @export
+#' @rdname body_sequence
+hand_with_fingers_splayed <- function() emoji( from_runes("U+1F590") )
+
+#' @export
+#' @rdname body_sequence
+raised_hand <- function() emoji( from_runes("U+270B") )
+
+#' @export
+#' @rdname body_sequence
+thumbs_up <- function() emoji( from_runes("U+1F44D") )
+
+#' @export
+#' @rdname body_sequence
+thumbs_down <- function() emoji( from_runes("U+1F44E") )
+
+#' @export
+#' @rdname body_sequence
+raised_fist <- function() emoji( from_runes("U+270A") )
+
+#' @export
+#' @rdname body_sequence
+oncoming_fist <- function() emoji( from_runes("U+1F44A") )
+
+#' @export
+#' @rdname body_sequence
+left_facing_fist <- function() emoji( from_runes("U+1F91B") )
+
+#' @export
+#' @rdname body_sequence
+right_facing_fist <- function() emoji( from_runes("U+1F91C") )
+
+#' @export
+#' @rdname body_sequence
+raised_back_of_hand <- function() emoji( from_runes("U+1F91A") )
+
+#' @export
+#' @rdname body_sequence
+waving_hand <- function() emoji( from_runes("U+1F44B") )
+
+#' @export
+#' @rdname body_sequence
+love_you_gesture <- function() emoji( from_runes("U+1F91F") )
+
+#' @export
+#' @rdname body_sequence
+writing_hand <- function() emoji( from_runes("U+270D") )
+
+#' @export
+#' @rdname body_sequence
+clapping_hands <- function() emoji( from_runes("U+1F44F") )
+
+#' @export
+#' @rdname body_sequence
+open_hands <- function() emoji( from_runes("U+1F450") )
+
+#' @export
+#' @rdname body_sequence
+raising_hands <- function() emoji( from_runes("U+1F64C") )
+
+#' @export
+#' @rdname body_sequence
+palms_up_together <- function() emoji( from_runes("U+1F932") )
+
+#' @export
+#' @rdname body_sequence
+folded_hands <- function() emoji( from_runes("U+1F64F") )
+
+#' @export
+#' @rdname body_sequence
+handshake <- function() emoji( from_runes("U+1F91D") )
+
+#' @export
+#' @rdname body_sequence
+nail_polish <- function() emoji( from_runes("U+1F485") )
+
+#' @export
+#' @rdname body_sequence
+ear <- function() emoji( from_runes("U+1F442") )
+
+#' @export
+#' @rdname body_sequence
+nose <- function() emoji( from_runes("U+1F443") )
+
+#' @export
+#' @rdname body_sequence
+footprints <- function() emoji( from_runes("U+1F463") )
+
+#' @export
+#' @rdname body_sequence
+eyes <- function() emoji( from_runes("U+1F440") )
+
+#' @export
+#' @rdname body_sequence
+eye <- function() emoji( from_runes("U+1F441") )
+
+#' @export
+#' @rdname body_sequence
+eye_in_speech_bubble <- function() emoji( from_runes("U+1F441 U+FE0F U+200D U+1F5E8 U+FE0F") )
+
+#' @export
+#' @rdname body_sequence
+brain <- function() emoji( from_runes("U+1F9E0") )
+
+#' @export
+#' @rdname body_sequence
+tongue <- function() emoji( from_runes("U+1F445") )
+
+#' @export
+#' @rdname body_sequence
+mouth <- function() emoji( from_runes("U+1F444") )
--- R/tones.R
+++ R/tones.R
@@ -2,24 +2,38 @@
 #' Skin tone modifier functions
 #'
 #' @param txt An emoji to alter skin tone to
+#' @param tone tone to apply
 #' @return an emoji with modified skin tone (if appropriate)
 #'
 #' @rdname skin_tones
 #' @export
-light <- function( txt ) emoji( paste0( txt, from_runes("U+1F3FB") ) )
+light_skin_tone <- function( txt ) emoji( paste0( txt, from_runes("U+1F3FB") ) )
 
 #' @rdname skin_tones
 #' @export
-medium_light <- function( txt ) emoji( paste0( txt, from_runes("U+1F3FC") ) )
+medium_light_skin_tone <- function( txt ) emoji( paste0( txt, from_runes("U+1F3FC") ) )
 
 #' @export
 #' @rdname skin_tones
-medium <- function( txt ) emoji( paste0( txt, from_runes("U+1F3FD") ) )
+medium_skin_tone <- function( txt ) emoji( paste0( txt, from_runes("U+1F3FD") ) )
 
 #' @export
 #' @rdname skin_tones
-medium_dark <- function(txt) emoji( paste0( txt, from_runes("U+1F3FE") ) )
+medium_dark_skin_tone <- function(txt) emoji( paste0( txt, from_runes("U+1F3FE") ) )
 
 #' @export
 #' @rdname skin_tones
-dark <- function(txt) emoji( paste0( txt, from_runes("U+1F3FF") ) )
+dark_skin_tone <- function(txt) emoji( paste0( txt, from_runes("U+1F3FF") ) )
+
+#' @export
+#' @rdname skin_tones
+skin_tone <- function(txt, tone = c("light", "medium_light", "medium", "medium_dark", "dark") ){
+  tone <- match.arg(tone)
+  switch( tone,
+    light = light_skin_tone(txt),
+    medium_light = medium_light_skin_tone(txt),
+    medium = medium_skin_tone(txt),
+    medium_dark = medium_dark_skin_tone(txt),
+    dark = dark_skin_tone(txt)
+  )
+}
